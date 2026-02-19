8a8e6937bcb94fb3c6162bc7c55a9c439212239d
Luke LaBonte <contact@lukelabonte.com>
2026-02-19 17:09:27 -0600

    Skip substitution text when parsing ingredients for search index
    - Add _in_sub_list state to ignore <ul class="sub-list"> content
    - Guard <li> start/end handlers to prevent sub-list items from
      resetting ingredient tracking state

--- generate_search_index.py
+++ generate_search_index.py
@@ -28,6 +28,7 @@ class RecipeParser(HTMLParser):
         self._in_ingredient_list = False
         self._in_ingredient_li = False
         self._in_ingredient_grams = False
+        self._in_sub_list = False
         self._current_meta_label = ""
         self._current_meta_value = ""
         self._current_ingredient = ""
@@ -48,13 +49,17 @@ class RecipeParser(HTMLParser):
 
         elif tag == "ul":
             class_attr = attrs_dict.get("class", "")
-            if "meta-list" in class_attr:
+            if "sub-list" in class_attr:
+                self._in_sub_list = True
+            elif "meta-list" in class_attr:
                 self._in_meta_list = True
             elif "ingredient-list" in class_attr:
                 self._in_ingredient_list = True
 
         elif tag == "li":
-            if self._in_meta_list:
+            if self._in_sub_list:
+                pass
+            elif self._in_meta_list:
                 self._in_meta_li = True
                 self._current_meta_label = ""
                 self._current_meta_value = ""
@@ -78,13 +83,17 @@ class RecipeParser(HTMLParser):
             self._in_subtitle = False
 
         elif tag == "ul":
-            if self._in_meta_list:
+            if self._in_sub_list:
+                self._in_sub_list = False
+            elif self._in_meta_list:
                 self._in_meta_list = False
             elif self._in_ingredient_list:
                 self._in_ingredient_list = False
 
         elif tag == "li":
-            if self._in_meta_li:
+            if self._in_sub_list:
+                pass
+            elif self._in_meta_li:
                 self._in_meta_li = False
                 label = self._current_meta_label.strip().rstrip(":").lower()
                 value = self._current_meta_value.strip()
@@ -119,7 +128,7 @@ class RecipeParser(HTMLParser):
         elif self._in_meta_li and not self._in_meta_strong:
             self._current_meta_value += data
 
-        elif self._in_ingredient_li and not self._in_ingredient_grams:
+        elif self._in_ingredient_li and not self._in_ingredient_grams and not self._in_sub_list:
             self._current_ingredient += data
 
 
